5cd1c1fa81c7390d6b3c2cc7f1c6ec7f04e4d2d6
iboylan <pipiel326@gmail.com>
2025-11-17 19:06:43 +0000

    elelelel

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -1,24 +1,26 @@
 # Objective:
 # Students will understand how to create, modify, and access elements in Python lists.
 
+# lists are MUTABLE!!!!!!!!!!!!!!!!!!!!!!!! meaning you can change whats in them
+
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
 
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
 
-my_list.append('grape')
-print(my_list)
+# my_list.append('grape')
+# print(my_list)
 
-my_list.pop(1)
-print(my_list)
+# my_list.pop(1)
+# print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
 
 
 # Practice Problems:
@@ -27,10 +29,38 @@ print(numbers)
 
 # Print the second and last item.
 
-# Add a new item using .append().
+# Add a new item using .append(). .extend() adds multiple items at6 once
 
-# Remove the first item using .pop(0).
+# Remove the first item using .pop(0). popping- removes and returns last item
 
 # Reverse your list using .reverse().
 
 # Create a list of 3 lists (matrix), and access the middle element.
+
+h = [324943894234, 243342889423, 32842384983248943324]
+print(h[-2])
+
+lalala = {
+    "mimimi":12318238
+
+}
+
+print(lalala["mimimi"])
+
+uueeee = [[h, h, h, h]]
+
+print(uueeee[:-2])
+
+lelele = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(lelele)
+print(lelele[-2])
+
+mimii = list(range(1,1001))
+print(mimii)
+mimii.pop()
+print(mimii)
