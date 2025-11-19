a3c104d9db7fd3e6be459872a4e34c885ad2cc4e
iboylan <pipiel326@gmail.com>
2025-11-19 18:29:28 +0000

    Lelelelel

---
+++ week 13  csp/setstuples.py
@@ -0,0 +1,21 @@
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+print(type(set1))
+set1.add(6)
+print(set1)
+set1.remove(2)
+print(set1)
+
+set2 = {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,5,4,5,5,5,5,5,5,5,5,6,7,7,7,7,7,7,7}
+print(set2)
+
+mimimimi = [1,2,3,3,4,4,5,6,7,8,9,0,10]
+print(mimimimi)
+print(type(mimimimi))
+
+lalala = (1,2,3,4,5,6,7,8,9,0,10,11,12,13,1,4)
+print(lalala)
+print(type(lalala))
+
+dzxcvfgbnm = "324er5tfgyhujuuuuuuujh 998893292801983 jedhdmr89314 uyjddddddddddfcftvf 98fy3tr58y98y4frrrrrrrrrrrrrrrrrrrr85vrg35t4354354tfv9gvu8uj87tg5fijijfijefro"
+print(dzxcvfgbnm * 500)
