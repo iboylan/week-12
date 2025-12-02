e6eb5c7734be132396109abd5685cf8c98578653
iboylan <pipiel326@gmail.com>
2025-12-02 18:33:55 +0000

    alaala

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -18,17 +18,17 @@
 
 # Examples:
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 
-print(matrix[1][2])    # 6
+# print(matrix[1][2])    # 6
 
-# List comprehension
-first_col = [row[0] for row in matrix]
-print(first_col)       # [1, 4, 7]
+# # List comprehension
+# first_col = [row[0] for row in matrix]
+# print(first_col)       # [1, 4, 7]
 
 
 
@@ -36,10 +36,25 @@ print(first_col)       # [1, 4, 7]
 
 # Build a matrix variable containing 3 lists of 3 numbers each.
 
+alallaa = [
+    [121321213,3243424,234243342234],
+    [90654,5493,"m"],
+    ["a","b","c"]
+]
+
 # Print the first list.
+
+print(alallaa[0])
 
 # Print the second item from the third list.
 
+print(alallaa[2][1])
+
 # Use a list comprehension to extract the last item from each sub-list.
+y = -1
+for x in alallaa:
+    print(alallaa[y+1][2])
+    y=y+1
 
 # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+
